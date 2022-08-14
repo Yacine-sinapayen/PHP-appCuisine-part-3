06bff272bd014486d64ae53212eedaa978d84c08
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-08-14 17:31:50 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+    include_once('variables.php');
+    include_once('functions.php');
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="home.php">Site de Recettes</a>
+    <div class="navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
